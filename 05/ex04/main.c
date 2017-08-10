56b5d2471c32ff7b8eeae9e83e1da390e5a4a782
Simon CORNAZ <scornaz@e1r2p10.42.fr>
2017-08-10 12:54:32 +0200

    add d5

---
+++ 05/ex04/main.c
@@ -0,0 +1,56 @@
+char *ft_strncpy(char *dest, char *src, unsigned int n);
+
+/* #include <stdio.h> */
+/* #include <string.h> */
+/* int main(void) */
+/* { */
+/*     char dest[10000] = "Hello World"; */
+/*     char src[] = "afada"; */
+    
+/*     printf("%s\n", ft_strncpy(dest, src, 1000)); */
+
+/*     char dest1[10000] = "Hello World"; */
+/*     char src1[] = "afada"; */
+    
+/*     printf("%s\n", strncpy(dest1, src1, 1000)); */
+/*     return (0); */
+    
+/* } */
+
+
+
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+	char src[] = "loulous";
+	int i = 0;
+
+	while (i < 30)
+	{
+		char dest[46] = "coucou les supers megas gigas";
+		char dest1[36] = "coucou les supers megas gigas";
+		printf("%s\n", ft_strncpy(dest, src, i));
+		printf("%s\n", ft_strncpy(dest1, src, i));
+		i += 5;
+	}
+
+	printf("\n \n");
+
+	i = 0;
+	while (i < 30)
+	{
+		char dest[46] = "coucou les supers megas gigas";
+		char dest1[36] = "coucou les supers megas gigas";
+		printf("%s\n", strncpy(dest, src, i));
+		printf("%s\n", strncpy(dest1, src, i));
+		i += 5;
+	}
+
+
+//	printf("%s\n", strcpy("coucou les", "loulous"));
+
+	return (0);
+}
+
