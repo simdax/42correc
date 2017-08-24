670435cd32f8c6fa34c4e30541b766940b1fa62e
simdax <simoncornaz@gmail.com>
2017-08-24 12:48:43 +0200

    11 better

--- 11/ex01/main.c
+++ 11/ex01/main.c
@@ -1,5 +1,5 @@
-#include "../../ft_list.h"
-#include "../../ex00/ft_create_elem.c"
+#include "../../../ex00/ft_list.h"
+#include "../../../ex00/ft_create_elem.c"
 #include <unistd.h>
 
 void   ft_list_push_back(t_list **begin_list, void *data);
@@ -8,6 +8,12 @@ t_list *ft_create_elem(void *data);
 int main()
 {
 	t_list	*list;
+
+	t_list *list_vide;
+
+	list_vide = NULL;
+	ft_list_push_back(&list_vide, "toi");
+	write(1, list_vide->data, 5);
 
 	list = ft_create_elem("coucou");
 	ft_list_push_back(&list, "toi");
